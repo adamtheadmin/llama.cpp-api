1c6a15289f1638ef3d51c917d2ab3ab1af327f21
Adam Fowler <adam@Adams-M1-Pro.local>
2023-05-04 21:23:49 -0600

    Initial Commit

---
+++ chat.ts
@@ -0,0 +1,97 @@
+import * as child_process from "child_process";
+import fs from 'fs'
+import {ChildProcessWithoutNullStreams} from "child_process";
+import {Readable} from "stream";
+import * as Buffer from "buffer";
+import config from './config.json';
+
+class PassThrough extends Readable {
+    _read(size) {}
+}
+
+const chatbot_path: string = config.llama_path;
+const pathParts: string[] = chatbot_path.split('/');
+const working_dir: string = pathParts.splice(0, pathParts.length - 1).join('/');
+
+try {
+    fs.accessSync(working_dir);
+    process.chdir(working_dir);
+} catch (e) {
+    throw new Error(`Could not find working directory: ${working_dir}. Please set the CHATBOT_PATH env variable.`);
+}
+
+interface QueueItem {
+    prompt: string;
+    response: PassThrough;
+}
+
+export default new class {
+    ready: boolean = false;
+    responseStream: PassThrough;
+    process: ChildProcessWithoutNullStreams;
+    queue:  QueueItem[] = [];
+
+    constructor() {
+        this.process = child_process.spawn(config.llama_path, [
+            '-m',
+            config.model_path,
+            ...config.llama_params
+        ]);
+        this.process.stdout.on('data', (bit:Buffer) => {
+            const str = bit.toString();
+            if (str.includes('>')) {
+                this.responseStream?.push(null);
+                this.responseStream = null;
+                if (this.queue.length) {
+                    const {prompt, response} = this.queue.shift();
+                    this.sendPrompt(prompt, response);
+                } else {
+                    this.ready = true;
+                }
+            }
+            if (this.responseStream) {
+                this.responseStream.push(str.replace('>', '').replace("\u001b[1m\u001b[32m\u001b[0m", ""));
+            }
+        });
+    }
+
+    private sendPrompt(prompt:string, useStream?: PassThrough):PassThrough {
+        console.log(`Q: ${prompt}`);
+        this.ready = false;
+        this.process.stdin.write(prompt + '\r\n');
+        this.responseStream = useStream ? useStream : new PassThrough();
+        process.stdout.write("A: ");
+        this.responseStream.on('data', chunk => process.stdout.write(chunk));
+        return this.responseStream;
+    }
+
+    private cleanseTerminalInput(input: string) :string {
+        return input.replace(/\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])/g, '').trim();
+    }
+
+    sanitizeInput(input: string): string {
+        return input.replace(/(\r\n|\n|\r)/gm, ' ');
+    }
+
+    ask(prompt:string):Readable {
+        prompt = this.sanitizeInput(prompt);
+        if (this.ready) {
+            return this.sendPrompt(prompt);
+        }
+        const stream = new PassThrough();
+        this.queue.push({
+            response: stream,
+            prompt
+        });
+        return stream;
+    }
+
+    async asyncAsk(prompt: string):Promise<string> {
+        const stream = this.ask(prompt);
+        let buffer = '';
+        stream.on('data', (bit: Buffer) => buffer += bit.toString());
+        return new Promise((resolve) => {
+           stream.on('end', () => resolve(this.cleanseTerminalInput(buffer)));
+        });
+    }
+}
---
+++ readme.md
@@ -0,0 +1,69 @@
+# LlaMA.cpp Chatbot Restful API
+
+*This was created using [alpaca api](https://github.com/adamtheadmin/alpaca-chatbot-api) as a base.*
+
+This is a chatbot request queue, and restful API for accessing the LLaMA.cpp repo. 
+This is designed to work with [llama.cpp](https://github.com/ggerganov/llama.cpp)
+
+Features:
+ - Prompt Queue
+ - Restful API
+ - Socket.io API
+
+### Config File Variables
+ - llama_path: The file path to the main executable of the Llama chatbot application.
+ - model_path: The file path to the pre-trained model used by the Llama chatbot.
+ - llama_params: A list of command line arguments and their values for configuring the LLaMA chatbot. These will be passed into the command line arguments into the chatbot.
+ - port: The port number on which the API service will listen for incoming requests, set to "8080".
+### Run
+- Install Typescript
+- `npm i`
+- `ts-node index.ts`
+
+### Install
+- Install Typescript
+- `cp config.example.json config.example.json`
+- `nano config.json`
+- `npm i`
+- `npm run build`
+- Install as a service, the run command would be `node index.js`. Be sure to have all the proper config values set.
+
+### API Usage
+To use the API, send a POST request to `/chat` with a payload that has a field named `prompt` that is a string value.
+
+    {"prompt": "How old was george washington when he died?"}
+
+You should get a response that looks like this:
+
+    {"response":"George Washington was 67 years old when he died on December 14, 1799."}
+
+### Socket.io Usage
+
+To use this with websockets, you should emit a `chat` event to the websocket server 
+with a `prompt` and an `id`. The `id` field will be returned later with responses.
+
+    socket.emit("chat", { prompt: input, id: 'remember me' });
+
+To get the chunks of the response, write some code that looks like this
+
+```
+socket.on("response", ({chunk, id}) => {
+    ...
+});
+```
+When the server is done sending the response, you will get an `endResponse` event with the `id` that was originally sent in.
+```
+socket.on("endResponse", ({id}) => {
+    ...
+});
+```
+
+### Licence
+
+Copyright 2023 Adam Fowler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
